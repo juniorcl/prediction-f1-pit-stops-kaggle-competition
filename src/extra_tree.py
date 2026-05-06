86455505cbc1f8cef8e26ae9351cfbac728b7b8d
Júnior <clebiojuniorcj@gmail.com>
2026-05-06 15:15:28 -0300

    first commit

---
+++ src/extra_tree.py
@@ -0,0 +1,106 @@
+#%%
+import optuna
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.inspection import permutation_importance
+from sklearn.model_selection import StratifiedKFold
+
+from feature_engine.selection import DropFeatures
+
+
+def dump_pickle(file_obj, file_path):
+    with open(file_path, 'bw') as file:
+        pickle.dump(file_obj, file)
+
+
+
+X_train = pd.read_parquet('../data/X_train.parquet')
+X_test = pd.read_parquet('../data/X_test.parquet')
+
+y_train = pd.read_parquet('../data/y_train.parquet')
+
+
+model = ExtraTreesClassifier(random_state=42, class_weight='balanced')
+model.fit(X_train, y_train.PitNextLap)
+
+perm_result = permutation_importance(
+    estimator=model, 
+    X=X_train, 
+    y=y_train.PitNextLap, 
+    n_jobs=-1, 
+    scoring='roc_auc'
+)
+
+importance_df = pd.DataFrame({
+    "feature": X_train.columns.tolist(),
+    "importance_mean": perm_result.importances_mean,
+    "importance_std": perm_result.importances_std
+}).sort_values(by="importance_mean", ascending=False)
+
+features_to_drop = importance_df.query("importance_mean <= 0").feature.tolist()
+
+
+def objective(trial, X, y):
+
+    model = make_pipeline(
+        [
+            DropFeatures(features_to_drop),
+            ExtraTreesClassifierClassifier(
+                n_estimators=trial.suggest_int("n_estimators", 200, 1200),
+                max_depth=trial.suggest_int("max_depth", 3, 30),
+                min_samples_split=trial.suggest_int("min_samples_split", 2, 20),
+                min_samples_leaf=trial.suggest_int("min_samples_leaf", 1, 10),
+                max_features=trial.suggest_categorical("max_features", ["sqrt", "log2", None]),
+                bootstrap=trial.suggest_categorical("bootstrap", [True, False]),
+                criterion=trial.suggest_categorical("criterion", ["gini", "entropy"]),
+                class_weight="balanced",
+                random_state=42,
+            )
+        ]
+    )
+    
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    aucs = []
+
+    for fold, (train_idx, valid_idx) in enumerate(cv.split(X, y)):
+
+        X_train, X_valid = X.iloc[train_idx, :], X.iloc[valid_idx, :]
+        y_train, y_valid = y.iloc[train_idx, 0], y.iloc[valid_idx, 0]
+        
+        model.fit(X_train, y_train)
+        proba = model.predict_proba(X_valid)[:, 1]
+        
+        auc = roc_auc_score(y_valid, proba)
+        aucs.append(auc)
+
+        trial.report(np.mean(aucs), step=fold)
+
+        if trial.should_prune():
+            raise optuna.exceptions.TrialPruned()
+
+    return np.mean(aucs)
+
+study = optuna.create_study(direction="maximize", pruner=optuna.pruners.MedianPruner(n_warmup_steps=2))
+study.optimize(lambda trial: objective(trial, X_train, y_train), n_trials=50, n_jobs=-1, show_progress_bar=True)
+
+
+pipe_tuned = make_pipeline(
+    [
+        DropFeatures(features_to_drop),
+        ExtraTreesClassifierClassifier(
+            class_weight="balanced",
+            random_state=42,
+            n_jobs=-1,
+            **study.best_params
+        )
+    ]
+)
+pipe_tuned.fit(X_train, y_train.PitNextLap)
+
+dump_pickle(pipe_tuned, '../models/model_random_forest.pkl')
